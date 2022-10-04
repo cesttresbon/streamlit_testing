ff875ffde6a69e8300e9e87c6b2c3e858b6a8b78
cesttresbon <115021784+cesttresbon@users.noreply.github.com>
2022-10-04 16:07:01 -0700

    Update webserver.py
    switched port to 80

--- webserver.py
+++ webserver.py
@@ -9,4 +9,4 @@ def read_root():
     return {"Hello": "World"}
   
 if __name__ == "__main__":
-      uvicorn.run("app.app:app",host='0.0.0.0', port=4557, reload=True, debug=True, workers=3)
+      uvicorn.run("app.app:app",host='0.0.0.0', port=80, reload=True, debug=True, workers=3)
